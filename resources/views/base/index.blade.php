09debe400c09d4b8a3a63e1ff4366ba1081e49bb
Ayanwoye Ayodeji <ayanwoye74@gmail.com>
2020-09-18 21:51:56 +0100

    new changes

--- resources/views/base/index.blade.php
+++ resources/views/base/index.blade.php
@@ -80,7 +80,7 @@
             </div>
         </div>
     </section>
-    <vodal :show="show" animation="rotate" :width="450" :height="450" @hide="show = false">
+    <vodal :show="show" animation="rotate" :width="450" :height="500" @hide="show = false">
         <div class="p-5">
             <h3 class="font-bold text-blue text-large">Please provide your details below</h3>
             <p class="my-2 text-medium"><span class="font-bold">NOTE</span>: This information will be used to create an account for you inorder to have a section with the Doctor.</p>
@@ -100,7 +100,7 @@
             <div class=" my-3 lg:mx-5 md:mx-5 mx-4 relative">
                 <input id="email" type="email" placeholder="Email address" class="text-medium bg-gray py-4 w-full pl-10 border-none focus:outline-none rounded-sm @error('email') is-invalid @enderror" email="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                 <div class="absolute top-0 left-0 ml-4" style="margin-top: .86rem">
-                    <i class="ri-user-line ri-fw text-gray-100"></i>
+                    <i class="ri-mail-line ri-fw text-gray-100"></i>
                 </div>
                 @if($errors->has('email'))
                     <span class="text-red" role="alert">
@@ -135,6 +135,17 @@
                         @endif
                     </div>
                 </div>
+            </div>
+            <div class=" my-3 lg:mx-5 md:mx-5 mx-4 relative">
+                <input id="date" type="date" placeholder="Date" class="text-medium bg-gray py-4 w-full pl-10 border-none focus:outline-none rounded-sm" name="date" required autocomplete="date" autofocus>
+                <div class="absolute top-0 left-0 ml-4" style="margin-top: .86rem">
+                    <i class="ri-user-line ri-fw text-gray-100"></i>
+                </div>
+                @if($errors->has('date'))
+                    <span class="text-red" role="alert">
+                        {{ $errors->first('date') }}
+                    </span>
+                @endif
             </div>
             <div class="mx-5">
                 <button type="submit" class="bg-orange text-white py-3 px-4 w-full text-medium">submit</button>
