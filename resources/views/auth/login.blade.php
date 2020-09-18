c2bea0a30cad8138da3e0498f4943adefdbcd708
Ayanwoye Ayodeji <ayanwoye74@gmail.com>
2020-09-18 20:40:22 +0100

    new changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,16 +2,13 @@
 
 @section('content')
 <div class="flex flex-wrap">
-    <div class="lg:w-1/2 md:w-1/2 w-full bg-blue" style="height:100vh">
-        <img src="{{ asset('images/auth-doc.svg') }}" alt="">
-    </div>
-    <div class="lg:w-1/2 md:w-1/2 w-full lg:py-48 md:py-24 px-20" style="height:100vh">
-        <div class="text-blue text-h4 font-bold mb-5 text-center">{{ __('Welcome') }}</div>
+    <div class="lg:w-1/2 md:w-1/2 w-full lg:py-48 md:py-24 py-32 lg:px-24 md:px-20 px-10 " style="height:100vh">
+        <div class="text-blue text-h4 font-bold mb-5 text-center">{{ __('Welcome, User') }}</div>
         <form method="POST" action="{{ route('login') }}" class="">
             @csrf
             <div>
                 <div class="w-full mb-5 relative">
-                    <input id="email" type="email" placeholder="Email Address" class="text-medium  rounded-full bg-gray py-4 w-full pl-10 border-none focus:outline-none rounded-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <input id="email" type="email" placeholder="Email Address" class="text-medium bg-gray py-4 w-full pl-10 border-none focus:outline-none rounded-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                     <div class="absolute top-0 left-0 ml-4" style="margin-top: .86rem">
                         <i class="ri-mail-line ri-fw text-gray-100"></i>
                     </div>
@@ -24,7 +21,7 @@
             </div>
 
             <div class="w-full mb-5 relative">
-                <input type="password" placeholder="*********" class="placeholder:text-medium placeholder:pt-6 text-medium rounded-full bg-gray py-4 w-full pl-10 border-none focus:outline-none rounded-lg @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" required autocomplete="password" autofocus>
+                <input type="password" placeholder="*********" class="placeholder:text-medium placeholder:pt-6 text-medium bg-gray py-4 w-full pl-10 border-none focus:outline-none rounded-lg @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" required autocomplete="password" autofocus>
                 <div class="absolute top-0 left-0 ml-4" style="margin-top: .65rem">
                     <i class="ri-lock-line ri-fw text-gray-100"></i>
                 </div>
@@ -34,11 +31,17 @@
                     </span>
                 @endif
             </div>
+            <p class="text-gray-dark mb-3 text-medium">
+                Don't have an account?
+                <a href="/register" class="text-blue underline">signup</a>
+            </p>
 
-            <div class="w-40 mx-auto">
-                <button class="bg-blue rounded-full text-white py-3 px-4 w-full text-medium">Login</button>
-            </div>
+            <button class="bg-orange text-white py-3 px-4 w-full" type="submit">login</button>
         </form>
+    </div>
+
+    <div class="lg:w-1/2 md:w-1/2 w-full lg:flex md:flex hidden" style="height:100vh">
+        <img src="{{ asset('images/auth-doc.svg') }}" alt="" class="mt-5">
     </div>
 </div>
 {{-- <div class="container">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,141 +2,76 @@
 
 @section('content')
 <div class="flex flex-wrap">
-    <div class="lg:w-1/2 md:w-1/2 w-full bg-blue" style="height:100vh">
-        <img src="{{ asset('images/auth-doc.svg') }}" alt="">
-    </div>
-    <div class="lg:w-1/2 md:w-1/2 w-full lg:py-20 md:py-24 px-20" style="height:100vh">
-        <div class="text-blue text-h4 mb-5">{{ __('Welcome to registration') }}</div>
-        <form method="POST" action="{{ route('login') }}" class="">
+    <div class="lg:w-1/2 md:w-1/2 w-full lg:py-40 md:py-24 px-20 py-32 lg:px-24 md:px-20 px-10 " style="height:100vh">
+        <div class="text-blue text-h3 font-semibold text-center">{{ __('Create an account') }}</div>
+        <p class="text-gray-dark mb-5 text-medium text-center">
+            Already have an account?
+            <a href="/login" class="text-blue underline">signin</a>
+         </p>
+        <form method="POST" action="{{ route('register') }}" class="">
             @csrf
-            <div class="shadow bg-white h-32 w-32 mb-4 cursor-pointer">
-
+            <div class="flex flex-wrap">
+                <div class="lg:w-1/2 md:w-1/2 w-full mb-4 relative">
+                    <div class="mr-2">
+                        <input id="name" type="name" placeholder="Full name" class="text-medium bg-gray py-4 w-full pl-10 border-none focus:outline-none rounded-sm @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        <div class="absolute top-0 left-0 ml-4" style="margin-top: .86rem">
+                            <i class="ri-user-line ri-fw text-gray-100"></i>
+                        </div>
+                        @if($errors->has('name'))
+                            <span class="text-red" role="alert">
+                                {{ $errors->first('name') }}
+                            </span>
+                        @endif
+                    </div>
+                    
+                </div>
+                <div class="lg:w-1/2 md:w-1/2 w-full mb-4 relative">
+                    <div class="mr-2">
+                        <input id="email" type="email" placeholder="Email Address" class="text-medium bg-gray py-4 w-full pl-10 border-none focus:outline-none rounded-sm @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <div class="absolute top-0 left-0 ml-4" style="margin-top: .86rem">
+                            <i class="ri-mail-line ri-fw text-gray-100"></i>
+                        </div>
+                        @if($errors->has('email'))
+                            <span class="text-red" role="alert">
+                                {{ $errors->first('email') }}
+                            </span>
+                        @endif
+                    </div>
+                    
+                </div>
             </div>
 
-            <div class="shadow bg-white h-32 w-32 mb-4 cursor-pointer">
-
-            </div>
-            {{-- <div>
-                <div class="w-full mb-5">
-                    <input id="email" type="email" placeholder="Email Address" class="text-medium bg-gray py-4 w-full px-3 border-none focus:outline-none rounded-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-        
-                    @if($errors->has('email'))
+            <div class="w-full mb-5 relative">
+                <div class="mr-2">
+                    <input type="password" placeholder="*********" class="placeholder:text-medium placeholder:pt-6 text-medium bg-gray py-4 w-full pl-10 border-none focus:outline-none rounded-lg @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" required autocomplete="password" autofocus>
+                    <div class="absolute top-0 left-0 ml-4" style="margin-top: .65rem">
+                        <i class="ri-lock-line ri-fw text-gray-100"></i>
+                    </div>
+                    @if($errors->has('password'))
                         <span class="text-red" role="alert">
-                            {{ $errors->first('email') }}
+                            {{ $errors->first('password') }}
                         </span>
                     @endif
                 </div>
             </div>
-
-            <div>
-                <div class="w-full mb-5">
-                    <input id="email" type="email" placeholder="Email Address" class="text-medium bg-gray py-4 w-full px-3 border-none focus:outline-none rounded-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-        
-                    @if($errors->has('email'))
-                        <span class="text-red" role="alert">
-                            {{ $errors->first('email') }}
-                        </span>
-                    @endif
-                </div>
+            <div class="w-full mb-5 flex ">
+               
+               <div class="">
+                    <input type="checkbox" name="password" requiredautofocus>
+               </div>
+               <div class="text-medium ml-3">
+                   <p>I've read and agree with Terms of Service and our Privacy policy</p>
+               </div>
+                    
+                
             </div>
 
-            <div>
-                <div class="w-full mb-5">
-                    <input id="email" type="email" placeholder="Email Address" class="text-medium bg-gray py-4 w-full px-3 border-none focus:outline-none rounded-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-        
-                    @if($errors->has('email'))
-                        <span class="text-red" role="alert">
-                            {{ $errors->first('email') }}
-                        </span>
-                    @endif
-                </div>
-            </div>
-
-            <div class="w-full mb-5">
-                <input type="password" placeholder="*********" class="text-medium bg-gray py-4 w-full px-3 border-none focus:outline-none rounded-lg @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" required autocomplete="password" autofocus>
-    
-                @if($errors->has('password'))
-                    <span class="text-red" role="alert">
-                        {{ $errors->first('password') }}
-                    </span>
-                @endif
-            </div> --}}
-
-            <button class="bg-orange text-white py-3 px-4 w-full">Submit</button>
+            <button type="submit" class="bg-orange text-white py-3 px-4 w-full">Register</button>
         </form>
     </div>
-</div>
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div class="lg:w-1/2 md:w-1/2 w-full lg:flex md:flex hidden" style="height:100vh">
+        <img src="{{ asset('images/auth-doc.svg') }}" alt="">
     </div>
-</div> --}}
+</div>
+
 @endsection
