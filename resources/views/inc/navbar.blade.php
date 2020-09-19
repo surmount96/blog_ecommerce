994f4c36371497691c0fccb3660efb168733869f
Ayanwoye Ayodeji <ayanwoye74@gmail.com>
2020-09-19 08:04:15 +0100

    new changes

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -10,7 +10,7 @@
         <li class="px-6"><a href="services">Service</a></li>
         <li class="px-6"><a href="/about">About</a></li>
         <li class="pl-6"><a href="/contact">Contact</a></li>
-        <li class="pl-6"><a href="/register" class="bg-orange text-white py-3 px-10 rounded">Register</a></li>
+        <li class="pl-6"><a href="/register" class="bg-orange text-medium text-white py-3 px-10">Register</a></li>
     </ul>
     <ul class="lg:hidden md:hidden flex">
         <li class="cursor-pointer" @click="">
