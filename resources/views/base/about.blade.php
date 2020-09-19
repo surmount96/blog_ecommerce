74a73c293a531775b45a71cc1a2fbb8bc3ea45e8
Ayanwoye Ayodeji <ayanwoye74@gmail.com>
2020-09-19 08:01:52 +0100

    new changes

--- resources/views/base/about.blade.php
+++ resources/views/base/about.blade.php
@@ -11,6 +11,12 @@
             <div class="my-4">
                 <img src="{{ asset('images/cancer.jpg') }} " alt="team" class="w-64 h-48">
             </div>
+            <div class="mt-4">
+                <button class="bg-orange py-3 px-3 text-medium text-white flex items-center">
+                    Check out or mechandise
+                    <i class="ri-arrow-right-line ri-fw text-white ml-3"></i>
+                </button>
+            </div>
         </div>
         <div class="lg:w-1/2 md:w-1/2 sm:w-1/2 w-full" style="background-image: url('{{ asset('images/lady.jpg') }}'); background-size:cover; background-position:center;background-repeat:no-repeat;height: 60vh;">
         </div>
